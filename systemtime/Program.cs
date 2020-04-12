9af1a947ef26ae991d367bc938c9f1c1aa3d5074
unknown <lktram1@gmail.com>
2020-04-12 21:45:46 +0700

    .

---
+++ systemtime/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace systemtime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localdate = DateTime.Now;
+            System.Console.WriteLine("DateTime Now is: " +localdate);
+        }
+    }
+}
